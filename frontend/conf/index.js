610b89f1d5c3b49a0b9bafb51f0291dbae9ea864
Vishal Kumar <kvishalrj2020@gmail.com>
2024-06-13 12:36:47 +0530

    update backend url

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,5 @@
 
-const config = { backendEndpoint: "http://43.204.212.36:8082" };
+// const config = { backendEndpoint: "http://43.204.212.36:8082" };
+const config = { backendEndpoint: "https://qtripdynamic-d0xt.onrender.com" };
 
 export default config;
